e963c89a1fa82600ffc73ce63c8b39e15b53f8ed
Ari Koeppel <44173466+ahdkoeppel@users.noreply.github.com>
2024-02-15 09:43:57 -0500

    Add as own function

---
+++ tima_psaturationpa_Sonntag1994.m
@@ -0,0 +1,19 @@
+function [Psat] = tima_psaturationpa_Sonntag1994(TempK)
+% TIMA_PSATURATIONPA_SONNTAG1994
+%   function to saturation vapor pressure at a given temperature
+%
+% Syntax
+%   [Psat] = tima_PSaturationPa_Sonntag1994(TempK)
+%
+% Inputs
+%   TempK: temperature (K)
+%
+% Outputs
+%   Psat: Saturation vapor pressure (Pa)
+%
+% Sources:
+%   Sonntag 1994
+%   http://cires1.colorado.edu/~voemel/vp.html
+
+Psat = 100.*exp(-6096.9385./TempK+16.635794-2.711193E-2.*TempK+1.673952E-5*TempK.^2+2.433502.*log(TempK)); %Pa
+end
